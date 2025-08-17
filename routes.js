e488b637dec16fd4b569996e001a639cb442a91b
Ipshita Bhardwaj <bhardwaj.ipshita0@gmail.com>
2025-08-17 21:48:28 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,45 @@
+// Bus routes and timings data
+const busRoutes = {
+  Chandigarh: {
+    5: {
+      driver: 'Trilochan Singh',
+      mobile: '8283025622',
+      stations: [
+        { sno: 1, station: 'DHANAS', time: '7:15 AM' },
+        { sno: 2, station: 'KHUDA LAHORA', time: '7:24 AM' },
+        { sno: 3, station: 'PGI', time: '7:28 AM' },
+        { sno: 4, station: 'PGI NEW OPD', time: '7:29 AM' },
+        { sno: 5, station: 'NAYAGAON BARRIER', time: '7:32 AM' },
+        { sno: 6, station: 'SECRETARIATE', time: '7:40 AM' },
+        { sno: 7, station: 'SECTOR-15-16 LIGHTS', time: '7:47 AM' },
+        { sno: 8, station: 'SECTOR-14-15 LIGHTS', time: '7:48 AM' },
+        { sno: 9, station: 'SECTOR-15-24 LIGHT', time: '7:51 AM' },
+        { sno: 10, station: 'SECTOR-23-24 LIGHT', time: '7:54 AM' },
+        { sno: 11, station: 'SECTOR-36-34 LIGHT', time: '7:58 AM' },
+        { sno: 12, station: 'SECTOR-37 BSNL LIGHT', time: '8:03 AM' },
+        { sno: 13, station: 'SECTOR-38 MARKET', time: '8:05 AM' },
+        { sno: 14, station: 'SECTOR-38 WEST', time: '8:08 AM' },
+        { sno: 15, station: 'MADANPUR CHOWNK', time: '8:10 AM' },
+        { sno: 16, station: 'PHASE-3/5 LIGHTS', time: '8:20 AM' },
+        { sno: 17, station: 'PHASE-4', time: '8:25 AM' },
+        { sno: 18, station: 'PHASE-5 GURUDWARA', time: '8:30 AM' },
+        { sno: 19, station: 'IVY HOSPITAL', time: '8:35 AM' },
+        { sno: 20, station: 'COLLEGE CAMPUS', time: '8:50 AM' }
+      ]
+    }
+  },
+  Panchkula: {},
+  Mohali: {},
+  Ropar: {},
+  Kurali: {},
+  Kharar: {},
+  Banur: {},
+  Rajpura: {},
+  'Mandi Gobindgarh': {},
+  Khanna: {},
+  Sirhind: {},
+  'Fatehgarh Sahib': {},
+  'Bassi Pathan': {},
+  Samrala: {},
+  Ludhiana: {}
+};
